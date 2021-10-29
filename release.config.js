5944dced2dba6dd0e7b3544ac470f577a0046af0
gunawanpras <gunawann.prasetio@gmail.com>
2021-10-29 23:24:58 +0700

    Run Prettier

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/gunawanpras/simple-react-app",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
-    ]
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/gunawanpras/simple-react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
