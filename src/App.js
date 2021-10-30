3e1d73f73d2a15d539b3cf6d474130a94c9e2378
gunawanpras <gunawann.prasetio@gmail.com>
2021-10-31 01:57:31 +0700

    fix: create issue

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Lets go Learn React with Me :|
+          Learn Go
         </a>
       </header>
     </div>
