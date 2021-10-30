8d28976cb24e741c2e3bfb3ecbc9bc1ca9ca230a
gunawanpras <gunawann.prasetio@gmail.com>
2021-10-31 02:08:32 +0700

    fix: the headline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Go
+          Learn Go Language
         </a>
       </header>
     </div>
