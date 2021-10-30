8493934dbc4936f1e82cc8a3169f715024b3b2ec
gunawanpras <gunawann.prasetio@gmail.com>
2021-10-30 12:47:12 +0700

    Upload release assets

--- release.config.js
+++ release.config.js
@@ -8,8 +8,8 @@ module.exports = {
       "@semantic-release/github",
       {
         assets: [
-          { path: "./build.zip", label: "Build" },
-          { path: "./coverage.zip", label: "Coverage" },
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
         ],
       },
     ],
